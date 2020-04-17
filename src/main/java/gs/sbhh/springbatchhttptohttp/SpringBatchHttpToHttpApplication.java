ee28c3891b312d6212c452e2f186694a0afbb1a4
gshaw-pivotal <gshaw+github@pivotal.io>
2020-04-17 15:17:43 -0400

    Initial spring app from spring initializr

---
+++ src/main/java/gs/sbhh/springbatchhttptohttp/SpringBatchHttpToHttpApplication.java
@@ -0,0 +1,13 @@
+package gs.sbhh.springbatchhttptohttp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBatchHttpToHttpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBatchHttpToHttpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gs/sbhh/springbatchhttptohttp/SpringBatchHttpToHttpApplicationTests.java
@@ -0,0 +1,13 @@
+package gs.sbhh.springbatchhttptohttp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBatchHttpToHttpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
